156026a9ca10a432b7a9de3a70bc5a20f9974d22
rrbeech <autumnartist@gmail.com>
2021-10-28 11:04:27 -0400

    New Tomorrowland

---
+++ Assets/LocomotionController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController TeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationthreshhold = 0.1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        TeleportRay.gameObject.SetActive(CheckIfActivated(TeleportRay)); 
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationthreshhold);
+        return isActivated;
+    }
+}
